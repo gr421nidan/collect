2ed6b801a38b7afbee8641f57afbdecabf7a955e
gr421nidan <nikushkina.dasha@yandex.ru>
2024-03-22 21:46:21 +0700

    Тесты для методов values, get

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -5,17 +5,18 @@ require_once ('src/Collect.php');
 require_once ('src/helpers.php');
 class CollectTest extends TestCase
 {
-    public function testCount()
-    {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
-    }
     public function testKeysAssociativeArray()
     {
         $array = ['a' => 1, 'b' => 2, 'c' => 3];
         $collect = new Collect\Collect($array);
         $keys = $collect->keys();
         $this->assertEquals(['a', 'b', 'c'], $keys->toArray());
+    }
+    public function testKeysNumericArray(){
+        $array = [1 => 'a', 2 => 'b', 3 => 'c'];
+        $collect = new Collect\Collect($array);
+        $keys = $collect->keys();
+        $this->assertEquals([1,2,3], $keys->toArray());
     }
     public function testKeysEmptyArray()
     {
@@ -25,21 +26,46 @@ class CollectTest extends TestCase
         $keys = $keysCollection->toArray();
         $this->assertEquals([], $keys);
     }
-    public function testKeysNumericArray(){
-        $array = [1 => 'a', 2 => 'b', 3 => 'c'];
-        $collect = new Collect\Collect($array);
-        $keys = $collect->keys();
-        $this->assertEquals([1,2,3], $keys->toArray());
-    }
-    public function testValues()
-    {
-        //Тест с числовыми ключами
-        $array = [1 => 'apple', 2 => 'banana', 3 => 'cherry'];
-        $collection = new Collect\Collect($array);
-        $valuesCollection = $collection->values();
-        $values = $valuesCollection->toArray();
-        $expectedValues = ['apple', 'banana', 'cherry'];
-        $this->assertEquals($expectedValues, $values);
 
+    public function testValuesAssociativeArray()
+    {
+        $array = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new Collect\Collect($array);
+        $keys = $collect->values();
+        $this->assertEquals([1, 2, 3], $keys->toArray());
     }
+    public function testValuesNumericArray()
+    {
+        $array = [1 => 'lion', 2 => 'cheetah', 3 => 'leopard'];
+        $collect = new Collect\Collect($array);
+        $values = $collect->values();
+        $expectedValues = ['lion', 'cheetah', 'leopard'];
+        $this->assertEquals($expectedValues, $values->toArray());
+    }
+    public function testValuesEmptyArray()
+    {
+        $array = [];
+        $collect = new Collect\Collect($array);
+        $values = $collect->values();
+        $this->assertEquals([], $values->toArray());
+    }
+
+    public function testGetWithKey()
+    {
+        $array = ['a' => 'hi!', 'b' => 'bye!'];
+        $collect = new Collect\Collect($array);
+        $result = $collect->get('a');
+        $this->assertEquals('hi!', $result);
+    }
+    public function testGetWithoutKey()
+    {
+        $array = ['a' => 'hi!', 'b' => 'bye!'];
+        $collect = new Collect\Collect($array);
+        $result = $collect->get();
+        $this->assertEquals($array, $result);
+    }
+
+
+
+
 }
