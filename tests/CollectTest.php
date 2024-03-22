1622a2fa7f6d1f34f2f9fccd0e59f925dd9b8a13
gr421nidan <nikushkina.dasha@yandex.ru>
2024-03-22 21:12:22 +0700

    Тесты для метода keys

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,7 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-
+require_once ('src/Collect.php');
+require_once ('src/helpers.php');
 class CollectTest extends TestCase
 {
     public function testCount()
@@ -9,5 +10,36 @@ class CollectTest extends TestCase
         $collect = new Collect\Collect([13,17]);
         $this->assertSame(2, $collect->count());
     }
+    public function testKeysAssociativeArray()
+    {
+        $array = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new Collect\Collect($array);
+        $keys = $collect->keys();
+        $this->assertEquals(['a', 'b', 'c'], $keys->toArray());
+    }
+    public function testKeysEmptyArray()
+    {
+        $array = [];
+        $collect = new Collect\Collect($array);
+        $keysCollection = $collect->keys();
+        $keys = $keysCollection->toArray();
+        $this->assertEquals([], $keys);
+    }
+    public function testKeysNumericArray(){
+        $array = [1 => 'a', 2 => 'b', 3 => 'c'];
+        $collect = new Collect\Collect($array);
+        $keys = $collect->keys();
+        $this->assertEquals([1,2,3], $keys->toArray());
+    }
+    public function testValues()
+    {
+        //Тест с числовыми ключами
+        $array = [1 => 'apple', 2 => 'banana', 3 => 'cherry'];
+        $collection = new Collect\Collect($array);
+        $valuesCollection = $collection->values();
+        $values = $valuesCollection->toArray();
+        $expectedValues = ['apple', 'banana', 'cherry'];
+        $this->assertEquals($expectedValues, $values);
 
+    }
 }
